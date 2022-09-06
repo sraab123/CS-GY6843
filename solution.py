34ee9b7d26ebc18eb8dc9c1c2f7e483fc0e3fc02
sraab123 <scraab1@gmail.com>
2022-09-06 06:35:28 -0400

    Assignment 1 test push from IDE

--- solution.py
+++ solution.py
@@ -16,13 +16,13 @@ def welcome_assignment_answers(question):
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
-        answer = "531af9e740b0953afb4fc9687987bc217e96f0d3"
+        answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "4"
+        answer = 4
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "2"
+        answer = 2
     else:
         answer = ""
     return answer
