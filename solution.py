35f099b7b37d232220fdcd50086d1c9fb51af646
sraab123 <scraab1@gmail.com>
2022-09-06 06:32:16 -0400

    Assignment 1 test push from IDE

--- solution.py
+++ solution.py
@@ -26,9 +26,3 @@ def welcome_assignment_answers(question):
     else:
         answer = ""
     return answer
-
-# Complete all the questions.
-# if __name__ == "__main__":
-#    # use this space to debug and verify that the program works
-#    debug_question = "Are encoding and encryption the same? - Yes/No"
-#    print(welcome_assignment_answers(debug_question))
